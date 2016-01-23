f22c3b2d8bfe9a11f9ca9b04410edea1c02bb200
Alex <alex.serebr@gmail.com>
2016-01-23 17:24:27 +0200

    Adds ScriptLine structure

---
+++ src/socks_core/script_line.rs
@@ -0,0 +1,66 @@
+//
+//   Copyright 2016 Alexander Serebryakov
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
+
+pub struct ScriptLine {
+    number : u64,
+    executable : bool,
+    data : String,
+    query : bool
+}
+
+
+impl ScriptLine {
+    pub fn new() -> ScriptLine {
+        ScriptLine {
+            number : 0,
+            executable : false,
+            data : String::new(),
+            query : false,
+        }
+    }
+
+    pub fn new_from_line(line : &String) -> ScriptLine {
+        let mut executable = false;
+
+        if line != "" {
+            executable = true;
+        }
+
+        ScriptLine {
+            number : 0,
+            executable : executable,
+            data : line.clone(),
+            query : false,
+        }
+    }
+
+    pub fn number(&self) -> u64 {
+        self.number
+    }
+
+    pub fn executable(&self) -> bool {
+        self.executable
+    }
+
+    pub fn data(&self) -> String {
+        self.data.clone()
+    }
+
+    pub fn query(&self) -> bool {
+        self.query
+    }
+}
